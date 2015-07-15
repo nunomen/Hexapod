eb5b8a8e237b9fee4fe9fdc65e6fa01ab1ea8864
ricardo73728 <rp457mail@gmail.com>
2015-07-15 18:01:08 +0100

    Class Packet
    Actualizar classe packet

--- src/packet.h
+++ src/packet.h
@@ -1,24 +1,30 @@
-#ifndef PMOTION_H
-#define PMOTION_H
+#ifndef PACKET_H
+#define PACKET_H
 
-using namespace std;
-
-class Packet {
-
-private:
-
-void *data; //data to transfer
-unsigned int data_size;
-unsigned char flag; 
+#include <stdint.h>
 
 
-public:
+class Packet
+{
+    public:
+        Packet(void);
+        Packet(void *data, uint8_t dataSize);
+        virtual ~Packet(void);
+        uint8_t get_size(void);          //receives void, returns the packet size in bytes
+        void set_size(uint8_t size); //receives data size, returns void
+        uint8_t get_flags(void);         //receives void, returns the flags byte
+        bool set_flag(uint8_t index);     //receives the flag index [0-7], returns true on success
+        bool reset_flag(uint8_t index);   //receives the flag index [0-7], returns true on success
+        bool is_flag_set(uint8_t index);  //receives the flag index [0-7], returns the flag
+        void *get_data(void);
 
-Packet();
-Packet(void*,unsigned int);
-~Packet();
+
+    protected:
+    private:
+        uint8_t size;
+        uint8_t flags;
+        void *data;
 
 };
 
-
-#endif
+#endif // PACKET_H
