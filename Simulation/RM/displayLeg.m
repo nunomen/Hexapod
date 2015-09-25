e713b26e507ee53c4fa19866a3c344684445aafc
okward <rikehmmc@gmail.com>
2015-09-25 17:20:12 +0100

    Minor changes, no bug correction
    Adventures and bug correction attempts, reorganized the code
    aesthetically

--- Simulation/RM/displayLeg.m
+++ Simulation/RM/displayLeg.m
@@ -1,26 +1,26 @@
 
-function [T01, T02, T03, T04, T05]=displayLeg(T01,  T02, T03, T04, T05, th2, th3, th4)
+function Tr=displayLeg(T, th2, th3, th4)
 
 %t1 t2 t3 : teta1 teta2 teta3
 %T1 T2 T3: transformation matrices 1 2 and 3
 
 %apply angles to transformation matrices
-%T01 is constant
-T02=apptransf(T02, th2, th3, th4);
-T03=apptransf(T03, th2, th3, th4);
-T04=apptransf(T04, th2, th3, th4);
-T05=apptransf(T05, th2, th3, th4);
+Tr{1}= T{1};                        %T01 is constant
+Tr{2}=apptransf(T{2}, th2, th3, th4);
+Tr{3}=apptransf(T{3}, th2, th3, th4);
+Tr{4}=apptransf(T{4}, th2, th3, th4);
+Tr{5}=apptransf(T{5}, th2, th3, th4);
 
 %plot the position vector
 figure;
 hold on;
 grid on;
 
-line([0 T01(1,4)],[0 T01(2,4)],[0 T01(3,4)],'Color','k'); %deixa yellow para debugk
-line([T01(1,4) T02(1,4)],[T01(2,4) T02(2,4)],[T01(3,4) T02(3,4)],'Color','y'); %yellow
-line([T02(1,4) T03(1,4)],[T02(2,4) T03(2,4)],[T02(3,4) T03(3,4)],'Color','r'); %red
-line([T03(1,4) T04(1,4)],[T03(2,4) T04(2,4)],[T03(3,4) T04(3,4)],'Color','g'); %green
-line([T04(1,4) T05(1,4)],[T04(2,4) T05(2,4)],[T04(3,4) T05(3,4)],'Color','b'); %blue
+line([0 Tr{1}(1,4)],[0 Tr{1}(2,4)],[0 Tr{1}(3,4)],'Color','k'); %black
+line([Tr{1}(1,4) Tr{2}(1,4)],[Tr{1}(2,4) Tr{2}(2,4)],[Tr{1}(3,4) Tr{2}(3,4)],'Color','y'); %yellow
+line([Tr{2}(1,4) Tr{3}(1,4)],[Tr{2}(2,4) Tr{3}(2,4)],[Tr{2}(3,4) Tr{3}(3,4)],'Color','r'); %red
+line([Tr{3}(1,4) Tr{4}(1,4)],[Tr{3}(2,4) Tr{4}(2,4)],[Tr{3}(3,4) Tr{4}(3,4)],'Color','g'); %green
+line([Tr{4}(1,4) Tr{5}(1,4)],[Tr{4}(2,4) Tr{5}(2,4)],[Tr{4}(3,4) Tr{5}(3,4)],'Color','b'); %blue
 
 xlabel('x');
 ylabel('y');
