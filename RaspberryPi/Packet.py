a2bc85b5b98959785185c11c65e2ed17087a2da1
littlebrat <gugas93@hotmail.com>
2016-01-30 18:48:45 +0000

    - Revisions for python3

--- RaspberryPi/Packet.py
+++ RaspberryPi/Packet.py
@@ -1,34 +1,36 @@
 class Packet:
-	def __init__(self):
-		self.data = {}
+    def __init__(self):
+        self.data = {}
 
-	#Associate a leg with angles to be moved in the next step
-	def setLeg(self,id,leg):
-		self.data.update({id : leg})
+    # Associate a leg with angles to be moved in the next step
+    def set_leg(self, number, leg):
+        self.data.update({number: leg})
 
-	#Get Leg id that will be moved
-	def getLeg(self,id):
-		return self.data[id]
+    # Get Leg id that will be moved
+    def leg(self, number):
+        return self.data[number]
 
-	#Return the string to be sent to arduino
-	def getPack(self):
-		flg = list('00000000')
-		for i in self.data.keys():
-			flg[8-i] = '1'
-		str = chr(222) + chr(230) + chr(222) + chr(int(''.join(flg),2))
-		for leg in self.data.keys():
-			vec=self.data[leg].getAngles()
-			for angle in vec:
-				str = str + chr(angle)
-		return str
+    # Return the string to be sent to arduino
+    def get_pack(self):
+        flg = list('00000000')
+        for i in self.data.keys():
+            flg[8-i] = '1'
+        pack = chr(222) + chr(230) + chr(222) + chr(int(''.join(flg), 2))
+        for leg in self.data.keys():
+            vec = self.data[leg].angles()
+            for angle in vec:
+                pack += chr(angle)
+        return pack
 
-#Class leg will be moved to the kinematics package
-class Leg: 
-	def __init__(self):
-		self.data = []
 
-	def setAngles(self,coxa,femur,tibia):
-		self.data.extend([coxa,femur,tibia])
+# Class leg will be moved to the kinematics package
+class Leg:
 
-	def getAngles(self):
-		return self.data
+    def __init__(self):
+        self.data = []
+
+    def set_angles(self, coxa, femur, tibia):
+        self.data.extend([coxa, femur, tibia])
+
+    def angles(self):
+        return self.data
--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -1,4 +1,4 @@
-import Packet,serial
+import Packet,serial, codecs
 
 joao = Packet.Packet()
 
@@ -8,23 +8,23 @@ pata3 = Packet.Leg()
 pata4 = Packet.Leg()
 pata5 = Packet.Leg()
 
-pata1.setAngles(30,20,20)
-pata2.setAngles(45,60,80)
-pata3.setAngles(20,20,90)
-pata4.setAngles(20,20,90)
-pata5.setAngles(20,20,90)
+pata1.set_angles(30,20,20)
+pata2.set_angles(45,60,80)
+pata3.set_angles(30,43,31)
+pata4.set_angles(19,41,42)
+pata5.set_angles(32,20,90)
 
-joao.setLeg(1,pata1)
-joao.setLeg(2,pata2)
-joao.setLeg(3,pata3)
-joao.setLeg(4,pata4)
-joao.setLeg(5,pata5)
+joao.set_leg(1,pata1)
+joao.set_leg(2,pata2)
+joao.set_leg(3,pata3)
+joao.set_leg(4,pata4)
+joao.set_leg(5,pata5)
 
-#Verify that the packet is well built
-for character in joao.getPack():
-  print character, character.encode('hex')
+# Verify that the packet is well built
+for character in joao.get_pack():
+    print(character, codecs.encode(int(character), 'hex'))
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 
-for x in joao.getPack():
-    ser.write(x)
+#for x in joao.get_pack():
+    #ser.write(x)
